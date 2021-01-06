f7605b63c97e0ff83139217966c5f26b492a8465
John Weingart <jrweingart@gmail.com>
2021-01-06 15:30:56 -0800

    Update to include all features

--- lib/trycourier.rb
+++ lib/trycourier.rb
@@ -1,10 +1,21 @@
+require "trycourier/events"
+require "trycourier/brands"
+require "trycourier/lists"
+require "trycourier/profiles"
+require "trycourier/session"
+require "trycourier/messages"
 require "trycourier/version"
 require "net/http"
 require "json"
 require "openssl"
 
 module Courier
+  # only exception for all other errors
+  class CourierAPIException < StandardError; end
+
+  # previous exceptions for send() (so it doesn't break)
   class ResponseError < StandardError; end
+
   class InputError < StandardError; end
 
   class SendResponse
@@ -18,49 +29,74 @@ module Courier
   end
 
   class Client
-    def initialize(auth_token = nil)
-      @auth_token = auth_token || ENV['COURIER_AUTH_TOKEN']
-      @uri = URI.parse('https://api.trycourier.app/send')
-      if @auth_token == nil or @auth_token == ""
-        raise InputError, "Client requires an auth_token be supplied."
+    def initialize(auth_token: nil, base_url: nil, username: nil, password: nil)
+      @base_url = if base_url
+        base_url
+      elsif ENV["COURIER_BASE_URL"]
+        ENV["COURIER_BASE_URL"]
+      else
+        "https://api.courier.com"
       end
+
+      @session = Courier::CourierAPISession.new
+
+      if auth_token
+        @session.init_token_auth(auth_token)
+      elsif ENV["COURIER_AUTH_TOKEN"]
+        @session.init_token_auth(ENV["COURIER_AUTH_TOKEN"])
+      elsif username && password
+        @session.init_basic_auth(username, password)
+      elsif ENV["COURIER_AUTH_USERNAME"] && ENV["COURIER_AUTH_PASSWORD"]
+        @session.init_basic_auth(ENV["COURIER_AUTH_USERNAME"], ENV["COURIER_AUTH_PASSWORD"])
+      end
+
+      @messages = Courier::Messages.new(@base_url, @session)
+      @profiles = Courier::Profiles.new(@base_url, @session)
+      @lists = Courier::Lists.new(@base_url, @session)
+      @events = Courier::Events.new(@base_url, @session)
+      @brands = Courier::Brands.new(@base_url, @session)
     end
 
     def send(body)
-      if not body.is_a?(Hash)
+      if !body.is_a?(Hash)
         raise InputError, "Client#send must be passed a Hash as first argument."
-      elsif body["event"] == nil
+      elsif body["event"].nil?
         raise InputError, "Must specify the 'event' key in Hash supplied to Client#send."
-      elsif body["recipient"] == nil
+      elsif body["recipient"].nil?
         raise InputError, "Must specify the 'recipient' key in Hash supplied to Client#send."
-      elsif body["data"] != nil and not body["data"].is_a?(Hash)
+      elsif !body["data"].nil? && !body["data"].is_a?(Hash)
         raise InputError, "The 'data' key in the Hash supplied to Client#send must also be a Hash."
-      elsif body["profile"] != nil and not body["profile"].is_a?(Hash)
+      elsif !body["profile"].nil? && !body["profile"].is_a?(Hash)
         raise InputError, "The 'profile' key in the Hash supplied to Client#send must also be a Hash."
       end
 
-      http = Net::HTTP.new(@uri.host, @uri.port)
-      http.use_ssl = true
-      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-
-      req = Net::HTTP::Post.new(@uri)
-      req["authorization"] = "Bearer #{@auth_token}"
-      req["content-type"] = "application/json"
-      req["User-Agent"] = "courier-ruby/#{Courier::VERSION}"
-      req.body = body.to_json
-
-      res = http.request(req)
+      url = @base_url + "/send"
+      res = @session.send(url, "POST", body: body)
       code = res.code.to_i
       obj = JSON.parse res.read_body
 
       if code == 200
         message_id = obj["messageId"]
-        return SendResponse.new(code, message_id)
-      elsif
-        message = obj["Message"] == nil ? obj["message"] : obj["Message"]
+        SendResponse.new(code, message_id)
+      elsif (message = obj["Message"].nil? ? obj["message"] : obj["Message"])
         err = "#{code}: #{message}"
         raise ResponseError, err
       end
     end
+
+    # getters for all class variables
+    attr_reader :base_url
+
+    attr_reader :session
+
+    attr_reader :messages
+
+    attr_reader :profiles
+
+    attr_reader :events
+
+    attr_reader :lists
+
+    attr_reader :brands
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,24 @@
 require "bundler/setup"
 require "trycourier"
+require "webmock/rspec"
+require "base64"
+
+AUTH_TOKEN_MOCK = "TESTAuthToken1234"
+AUTH_USERNAME_MOCK = "Jane"
+AUTH_PASSWORD_MOCK = "Doe"
+NOTIFICATION_ID = "TESTNotificationID"
+RECIPIENT_ID = "TESTRecipientID"
+LIST_ID = "TESTListID"
+BRAND_ID = "TESTBrandID"
+MESSAGE_ID = "TESTMessageID"
+EVENT_ID = "TESTEventID"
+DEFAULT_COURIER_URL = "https://api.courier.com"
+TOKEN_AUTH_HEADERS = {
+  "Authorization" => "Bearer " + AUTH_TOKEN_MOCK,
+  "Content-Type" => "application/json",
+  "Host" => "api.courier.com",
+  "User-Agent" => "courier-ruby/1.0.1"
+}
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
