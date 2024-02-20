9dc62283b08f1c0e06a93f5d638d6db5668d920d
fern <126544928+fern-bot@users.noreply.github.com>
2024-02-20 13:47:09 -0500

    Update gemconfig.rb

--- lib/gemconfig.rb
+++ lib/gemconfig.rb
@@ -3,10 +3,10 @@
 module Courier
   module Gemconfig
     VERSION = ""
-    AUTHORS = [""].freeze
-    EMAIL = ""
-    SUMMARY = ""
-    DESCRIPTION = ""
+    AUTHORS = ["Courier"].freeze
+    EMAIL = "support@courier.com"
+    SUMMARY = "Wraps calls to the Courier REST API"
+    DESCRIPTION = "Courier is the smartest way to design & deliver notifications. Design your notifications once, deliver them to any channel through one API."
     HOMEPAGE = "https://github.com/trycourier/courier-ruby"
     SOURCE_CODE_URI = "https://github.com/trycourier/courier-ruby"
     CHANGELOG_URI = "https://github.com/trycourier/courier-ruby/blob/master/CHANGELOG.md"
