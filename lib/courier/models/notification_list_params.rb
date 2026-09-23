3dcbe9a2e675adbef3b194da103ec442d5712dd1
courier-codegen <codegen@users.noreply.github.com>
2026-09-23 23:14:54 +0000

    feat(api): filter notification templates by tag [C-21172]

--- lib/courier/models/notification_list_params.rb
+++ lib/courier/models/notification_list_params.rb
@@ -25,12 +25,25 @@ module Courier
       #   @return [Boolean, nil]
       optional :notes, Courier::Internal::Type::Boolean, nil?: true
 
-      # @!method initialize(cursor: nil, event_id: nil, notes: nil, request_options: {})
+      # @!attribute tags
+      #   Comma-delimited list of tag names. Only templates carrying all of the listed
+      #   tags are returned. Matching is case-insensitive. Filtering is applied before
+      #   pagination.
+      #
+      #   @return [String, nil]
+      optional :tags, String
+
+      # @!method initialize(cursor: nil, event_id: nil, notes: nil, tags: nil, request_options: {})
+      #   Some parameter documentations has been truncated, see
+      #   {Courier::Models::NotificationListParams} for more details.
+      #
       #   @param cursor [String, nil] Opaque pagination cursor from a previous response. Omit for the first page.
       #
       #   @param event_id [String] Filter to templates linked to this event map ID.
       #
       #   @param notes [Boolean, nil] Include template notes in the response. Only applies to legacy templates.
+      #
+      #   @param tags [String] Comma-delimited list of tag names. Only templates carrying all of the listed tag
       #
       #   @param request_options [Courier::RequestOptions, Hash{Symbol=>Object}]
     end
